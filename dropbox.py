1aec4c61ca8d48126095981ec9f2f4ef01896e2f
Ryan Barrett <git@ryanb.org>
2013-10-01 22:33:52 -0700

    add skeleton for interactive mock backend for dashboard
    on app engine right now, based on https://github.com/snarfed/freedom, just to start to get something up and running that we can try out real oauth flows with. we can definitely switch to something else later if we want.
    
    starting to deploy at http://asheville-io.appspot.com/ , hidden from search engines.

---
+++ dropbox.py
@@ -0,0 +1,189 @@
+"""Dropbox destination.
+
+https://www.dropbox.com/developers/core/start/python
+https://www.dropbox.com/static/developers/dropbox-python-sdk-1.6-docs/
+https://www.dropbox.com/developers/core/docs
+"""
+
+__author__ = ['Ryan Barrett <freedom@ryanb.org>']
+
+import json
+import logging
+import os
+import string
+import StringIO
+import urllib
+
+import appengine_config
+from python_dropbox.client import DropboxOAuth2Flow, DropboxClient
+import models
+from webob import exc
+
+from google.appengine.api import urlfetch
+from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+import webapp2
+
+
+TITLE_MAX_LEN = 40
+REAL_DOMAIN = 'www.freedom.io'
+APPSPOT_DOMAIN = 'freedom-io-app.appspot.com'
+DROPBOX_APP_KEY = appengine_config.read('dropbox_app_key')
+DROPBOX_APP_SECRET = appengine_config.read('dropbox_app_secret')
+OAUTH_CALLBACK = 'https://%s/dropbox/oauth_callback' % APPSPOT_DOMAIN
+CSRF_PARAM = 'dropbox-auth-csrf-token'
+
+
+class DropboxCsrf(db.Model):
+  """Stores a CSRF token for the Dropbox OAuth2 flow."""
+  token = db.StringProperty(required=False)
+
+
+class Dropbox(models.Destination):
+  """A Dropbox account. The key name is the user id."""
+
+  # OAuth2 access token for this account
+  # https://www.dropbox.com/developers/core/start/python#authenticating
+  oauth_token = db.StringProperty(required=True)
+
+  def hostname(self):
+    return self.key().name()
+
+  def display_name(self):
+    return self.hostname()
+
+  @classmethod
+  def new(cls, user_id, **kwargs):
+    """Creates and saves a Dropbox entity based on query parameters.
+
+    Args:
+      user_id: string
+      kwargs: passed through to the Dropbox() constructor
+
+    Returns: Dropbox
+    """
+    return Dropbox.get_or_insert(user_id, **kwargs)
+
+  def publish_post(self, post):
+    """Writes a post to a file in Dropbox"""
+    activity = post.to_activity()
+    path = self.make_path(post, activity)
+
+    # https://www.dropbox.com/developers/core/start/python#toc-uploading
+    client = DropboxClient(self.oauth_token)
+    pretty_json = json.dumps(activity, indent=2)
+    response = client.put_file(path + '.json', StringIO.StringIO(pretty_json),
+                               overwrite=True)
+    logging.info('Wrote JSON post: %s', response)
+
+    html = post.render_html()
+    response = client.put_file(path + '.html', StringIO.StringIO(html),
+                               overwrite=True)
+    logging.info('Wrote HTML post: %s', response)
+
+    image = activity['object'].get('image', {}).get('url')
+    if image:
+      resp = urlfetch.fetch(image)
+      ext = os.path.splitext(image)[-1]
+      response = client.put_file(path + ext, StringIO.StringIO(resp.content),
+                                 overwrite=True)
+      logging.info('Wrote image: %s', response)
+
+  def publish_comment(self, comment):
+    """TODO"""
+    raise NotImplementedError()
+
+  def make_path(self, migratable, activity):
+    """Generates the file path for a post or comment, *without* extension."""
+    source = migratable.migration.source().type_display_name()
+
+    # Extract just the date, discard time and time zone
+    date = (activity.get('published', '')
+            or activity['object'].get('published', ''))[:10]
+
+    source_id = activity.get('id', '').split(':')[-1]
+
+    truncated = activity.get('title', '').strip()[:TITLE_MAX_LEN]
+    title = ''.join(c for c in truncated.replace(' ', '_')
+                    if c.isalnum() or c == '_')
+
+    return os.path.join('/', source, '_'.join((date, source_id, title)))
+
+
+# TODO: unify with other dests, sources?
+class AddDropbox(webapp2.RequestHandler):
+  def post(self):
+    csrf = DropboxCsrf()
+    csrf.save()
+    csrf_holder = {}
+    flow = DropboxOAuth2Flow(DROPBOX_APP_KEY, DROPBOX_APP_SECRET,
+                             OAUTH_CALLBACK, csrf_holder, CSRF_PARAM)
+
+    auth_url = flow.start(url_state=str(csrf.key().id()))
+
+    csrf.token = csrf_holder[CSRF_PARAM]
+    csrf.save()
+    logging.info('Stored DropboxCsrf id %d, redirecting to Dropbox: %s',
+      csrf.key().id(), auth_url)
+    self.redirect(auth_url)
+
+
+class OAuthCallback(webapp2.RequestHandler):
+  """OAuth callback. Fetches the user's blogs and re-renders the front page."""
+
+  def get(self):
+    # Redirect from https://freedom-io-app.appspot.com/ to http://freedom.io/
+    # for now, since App Engine charges $40/mo for SSL on custom domains:
+    # https://developers.google.com/appengine/docs/billing#Billable_Resource_Unit_Costs
+    if self.request.host == APPSPOT_DOMAIN:
+      self.redirect('http://%s%s' % (REAL_DOMAIN, self.request.path_qs))
+      return
+
+    # lookup the CSRF token
+    csrf_id = self.request.get('state').split('|')[1]
+    csrf = DropboxCsrf.get_by_id(int(csrf_id))
+    if not csrf:
+      raise exc.HTTPBadRequest('No CSRF token for id %s', csrf_id)
+
+    # extract the OAuth access token
+    csrf_holder = {CSRF_PARAM: csrf.token}
+    flow = DropboxOAuth2Flow(DROPBOX_APP_KEY, DROPBOX_APP_SECRET,
+                             OAUTH_CALLBACK, csrf_holder, CSRF_PARAM)
+    try:
+      access_token, user_id, _ = flow.finish(self.request.params)
+    except Exception, e:
+      logging.exception('Error finishing OAuth flow')
+      if isinstance(e, (DropboxOAuth2Flow.NotApprovedException,
+                        DropboxOAuth2Flow.BadStateException)):
+        self.redirect('/?msg=Error')
+        return
+      elif isinstance(e, (DropboxOAuth2Flow.CsrfException,
+                          DropboxOAuth2Flow.ProviderException)):
+        raise exc.HTTPForbidden()
+      elif isinstance(e, DropboxOAuth2Flow.BadRequestException):
+        raise exc.HTTPBadRequest()
+      else:
+        raise
+
+    logging.info('Storing new Dropbox account: %s', user_id)
+    dropbox = Dropbox.new(user_id, oauth_token=access_token)
+
+    # redirect so that refreshing the page doesn't try to regenerate the oauth
+    # token, which won't work.
+    self.redirect('/?dest=%s#sources' % str(dropbox.key()))
+
+
+class DeleteDropbox(webapp2.RequestHandler):
+  def post(self):
+    site = Dropbox.get(self.request.params['id'])
+    # TODO: remove tasks, etc.
+    msg = 'Deleted %s: %s' % (site.type_display_name(), site.display_name())
+    site.delete()
+    self.redirect('/?msg=' + msg)
+
+
+application = webapp2.WSGIApplication([
+    ('/dropbox/dest/add', AddDropbox),
+    ('/dropbox/oauth_callback', OAuthCallback),
+    ('/dropbox/dest/delete', DeleteDropbox),
+    ], debug=appengine_config.DEBUG)
