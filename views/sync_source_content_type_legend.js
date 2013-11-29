a30ecfbe88c09414b7e604b1064c973cea521f1b
Mark Hendrickson <markmhendrickson@gmail.com>
2013-11-29 15:19:47 +0100

    Make minor object call cleanups and change Ember Data library
    - Clean up more chained Ember.Object.get() calls
    - Use canary build of Ember Data library instead of beta build

--- views/sync_source_content_type_legend.js
+++ views/sync_source_content_type_legend.js
@@ -23,8 +23,8 @@ App.SyncSourceContentTypeLegendView = Ember.View.extend({
   }.property('contentType.totalItemsSynced', 'contentType.totalItemsAvailable'),
 
   syncedPercentage: function() {
-    if (this.get('contentType').get('totalItemsSynced') && this.get('contentType').get('totalItemsAvailable')) {
-      return Math.round(this.get('contentType').get('totalItemsSynced') / this.get('contentType').get('totalItemsAvailable') * 100) + '%';
+    if (this.get('contentType.totalItemsSynced') && this.get('contentType.totalItemsAvailable')) {
+      return Math.round(this.get('contentType.totalItemsSynced') / this.get('contentType.totalItemsAvailable') * 100) + '%';
     } else {
       return 'N/A';
     }
