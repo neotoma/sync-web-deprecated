fded8e4d4ef021fa1a0d0eed103adaf100ad8654
Mark Hendrickson <markmhendrickson@gmail.com>
2014-08-28 23:32:01 -0700

    Fix and clean up sources route

--- app/controllers/sources.js
+++ app/controllers/sources.js
@@ -1,62 +1,28 @@
 App.SourcesController = Ember.ArrayController.extend({
   needs: 'session',
-  isSaving: null,
-
-  totalAuthedSources: function() {
-    var total = 0;
-
-    this.get('model').forEach(function(source) {
-      if (source.get('authed')) {
-        total = total + 1;
-      }
-    });
-
-    return total;
-  }.property('model.@each.authed'),
-
-  noAuthedSources: function() {
-    return this.get('totalAuthedSources') ? false : true;
-  }.property('totalAuthedSources'),
-
-  totalEnabledContentTypes: function() {
-    var total = 0;
-
-    this.get('model').forEach(function(source) {
-      if (source.get('authed')) {
-        total = total + source.get('totalContentTypes');
-      }
-    });
-
-    return total;
-  }.property('model.@each.authed'),
-
-  hasEnabledContentType: function() {
-    return (this.get('totalEnabledContentTypes')) ? true : false;
-  }.property('totalEnabledContentTypes'),
+  sessionUser: Ember.computed.alias('controllers.session.user'),
 
   isDisabled: function() {
-     return !this.get('totalEnabledContentTypes');
-  }.property('totalEnabledContentTypes'),
+    return !this.get('sessionUser.hasUserSourceAuth');
+  }.property('sessionUser.hasUserSourceAuth'),
 
-  syncSourcesLabel: function() {
-    if (!this.get('controllers.session.model.user.hasSource')) {
-      if (this.get('isSaving')) {
-          return 'Loading...';
-      } else {
-          return 'Start Backing Up';
+  totalContentTypes: function() {
+    var userSourceIds = this.get('sessionUser.userSourceAuths').map(function(userSourceAuth) { return userSourceAuth.get('source.id'); });
+
+    var totalContentTypes = 0;
+
+    this.get('model').forEach(function(source) {
+      if (userSourceIds.indexOf(source.id) != -1) {
+        totalContentTypes = source.get('contentTypes.length');
       }
-    } else {
-      if (this.get('isSaving')) {
-          return 'Saving...';
-      } else {
-          return 'Save Changes';
-      }
-    }
-  }.property('isSaving'),
+    });
+
+    return totalContentTypes;
+  }.property('sessionUser.userSourceAuths'),
 
   actions: {
-    syncSources: function() {
-      $.post(APP_CONFIG.DATA.HOST + '/sources/sync');
+    proceed: function() {
+      //$.post(APP_CONFIG.DATA.HOST + '/sources/sync');
       this.transitionToRoute('sync');
     }
   }
