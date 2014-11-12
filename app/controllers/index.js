065db1265237d4ca13c298cc0b91df1a9282250e
Mark Hendrickson <markmhendrickson@gmail.com>
2014-11-12 23:36:15 +0100

    Add base animations for index page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,6 +15,47 @@ App.IndexController = Ember.ObjectController.extend({
   }.property('sessionUser.hasUserStorageAuth'),
 
   actions: {
+    startIcons: function() {
+      var spawnIcon = function() {
+        var target = $('#target');
+
+        var min_x = 1;
+        var min_y = 10;
+        var max_x = $('body').width();
+        var max_y = target.position().top - 10;
+
+        var left = (Math.floor(Math.random() * (max_x - min_x + 1) + min_x));
+        var top = (Math.floor(Math.random() * (max_y - min_y + 1) + min_y));
+
+        var icon = $('<div class="icon"></div>').attr({
+          'left': left,
+          'top': top
+        });
+
+        $('#index').append(icon);
+        
+        var bezier_params = {
+          start: { 
+            x: left, 
+            y: top, 
+            angle: 10
+          },  
+          end: { 
+            x: target.position().left + target.width() / 2 - icon.width() / 2,
+            y: target.position().top + target.height() / 2 - icon.height() / 2, 
+            angle: -10, 
+            length: 0.25
+          }
+        }
+
+        icon.animate({ path : new $.path.bezier(bezier_params)}, 1000, 'swing');
+        setTimeout(function() {
+          icon.remove();
+        }, 1100);
+      }
+
+      setInterval(spawnIcon, 1000);
+    },
     toggleDropboxUserStorageAuth: function() {
       if (!this.get('sessionUser.hasUserStorageAuth')) {
         this.get('controllers.session').authenticate();
